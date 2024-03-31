52fa782c20f138cae45ff3946257e38759af6501
Igor Krysin <igor_krisin@mail.ru>
2024-03-31 20:46:08 +0300

    HW 1

---
+++ Module 2 UIKit/HW2  Module 2/HW2  Module 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2  Module 2
+//
+//  Created by Игорь Крысин on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
